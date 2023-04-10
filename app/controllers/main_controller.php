c4250101a8d39ba6022a3921df248530304d9cff
andrew <andreyhorolsky@gmail.com>
2023-04-10 12:56:22 +0300

    login

--- app/controllers/main_controller.php
+++ app/controllers/main_controller.php
@@ -2,18 +2,36 @@
 
 class Main_controller{
 
+
+    //открывает страницу по умолчанию - index.html
     function defaultPage(){
 
         include("app/views/index.html");
 
     }
 
+
+    //выполняет регистрацию нового акк
     function register(){
 
         require("app/models/account_model.php");
         $model = new Account_model();
 
         $model->registration();
+
+    }
+
+
+    //выполняет вход в существующий акк
+    //выводит сообщение об удачном / неудачном входе
+    function login(){
+
+        require("app/models/account_model.php");
+        $model = new Account_model();
+
+        if ($model->login() == 1){
+            echo "login";
+        } else echo "error wit login";
 
     }
 }
--- app/models/account_model.php
+++ app/models/account_model.php
@@ -3,24 +3,40 @@
 class Account_model{
     static private $table = "accounts";
 
+
+    //регистрация нового пользователя
+    //ничего не возвращает
     function registration(){
         $pdo = new PDO("mysql:host=localhost;dbname=eatNormal", "root", "123");
         
-        $stmt = $pdo->prepare("INSERT INTO `accounts` (`id`, `name`, `birth`, `sex`, `goal`, `problem`, `email`, `password`) VALUES (NULL, ':name', ':birth', ':sex', ':goal', ':problem', ':email', ':pass')");
-
-        // $name = $_POST["name"];
-        // $sex = ($_POST["sex"] == "men")? "m":"w";
+        $stmt = $pdo->prepare("INSERT INTO `accounts` (`id`, `name`, `birth`, `sex`, `goal`, `problem`, `email`, `password`) VALUES (NULL, :name, :birth, :sex, :goal, :problem, :email, :password)");
+        
         $stmt->bindParam(":name", $_POST["name"]);
         $stmt->bindParam(":birth", $_POST["birth"]);
         $stmt->bindParam(":sex", $_POST["sex"]);
         $stmt->bindParam(":goal", $_POST["goal"]);
         $stmt->bindParam(":problem", $_POST["problem"]);
         $stmt->bindParam(":email", $_POST["email"]);
-        $stmt->bindParam(":pass", $_POST["password"]);
-
-        echo json_encode($_POST);
+        $stmt->bindParam(":password", $_POST["password"]);
 
         $stmt->execute();
+
+    }
+
+
+    //вход в существующий акк
+    //возвращает кол-во подходящих строк
+    function login(){
+        $pdo = new PDO("mysql:host=localhost;dbname=eatNormal", "root", "123");
+
+        $stmt = $pdo->prepare("SELECT `name` FROM `accounts` WHERE `email`=:email AND `password`=:password");
+
+        $stmt->bindParam(":email",  $_POST["email"]);
+        $stmt->bindParam(":password", $_POST["password"]);
+
+        $stmt->execute();
+
+        return $stmt->rowCount();
 
     }
 }
