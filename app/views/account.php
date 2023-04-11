33ba901265c6cd61a987cf89889c610dcd9c0aa6
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-04-11 15:34:12 +0300

    registration and main

--- app/views/account.php
+++ app/views/account.php
@@ -18,36 +18,10 @@
 </head>
 <body>
     <div class="wrapper">
-        <!-- общая оболочка шапки меню -->
-        <header id="header" class="header">
-            <!-- ограничивающий контейнер -->
-            <div class="header_container _container">
-                <nav class="header_menu">
-                    <ul class="menu_list">
-                        <li class="menu_item"> 
-                            <a href="" class="menu_link">Форум</a>
-                        </li>
-                        <li class="menu_item">
-                            <a href="" class="menu_link">Тесты</a>
-                        </li>
-                        <li class="menu_item">
-                            <a href="" class="menu_link">Консультации</a>
-                        </li>
-                        <li class="menu_item">
-                            <a href="" class="menu_link">Самопомощь</a>
-                        </li>
-                        <li class="menu_item">
-                            <img src="/images/avtorisation.svg" alt="">
-                            <a href="#popup" class="menu_link">Авторизация</a>
-                        </li>
-                        <li class="menu_item">
-                            <img src="/images/linza.svg" alt="">
-                            <a href="" class="menu_link">Поиск</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+
+        <?
+            include("app/views/menu.php");
+        ?>
             
         <div class="about-yourself">
             <div class="about-yourself__wrapper _container">
--- app/views/menu.php
+++ app/views/menu.php
@@ -6,6 +6,9 @@
                <nav class="header_menu">
                     <ul class="menu_list">
                         <li class="menu_item"> 
+                            <a href="/main/" class="menu_link">Главная</a>
+                        </li>
+                        <li class="menu_item"> 
                             <a href="" class="menu_link">Форум</a>
                         </li>
                         <li class="menu_item">
@@ -24,11 +27,7 @@
                                     //вывод имени если оно есть, иначе вывод Авторизация
                                     echo Account_model::getAccountName() ?? "Авторизация";
                                 ?>
-                                </a>
-                        </li>
-                        <li class="menu_item">
-                            <img src="/images/linza.svg" alt="">
-                            <a href="" class="menu_link">Поиск</a>
+                            </a>
                         </li>
                     </ul>
                </nav>
