57902415f95dcec8065f978a1e5d210f4aa46a4b
andrew <andreyhorolsky@gmail.com>
2023-04-12 20:33:08 +0300

    reg and log from main to reg

--- app/controllers/main_controller.php
+++ app/controllers/main_controller.php
@@ -12,34 +12,4 @@ class Main_controller extends Controller{
 
     }
 
-
-    //выполняет регистрацию нового акк
-    function register(){
-
-        require("app/models/account_model.php");
-        $this->model = new Account_model();
-
-        $this->model->registration();
-        $this->model->login();
-
-        header("Location: /main/");
-        exit();
-
-    }
-
-
-    //выполняет вход в существующий акк
-    //выводит сообщение об удачном / неудачном входе
-    function login(){
-        
-        require("app/models/account_model.php");
-        $this->model = new Account_model();
-
-        $this->model->login();
-        
-        header("Location: /main/");
-        exit();
-
-    }
-
 }
--- app/controllers/registration_controller.php
+++ app/controllers/registration_controller.php
@@ -7,4 +7,35 @@ class Registration_controller extends Controller{
     function defaultPage(){
         $this->view->render("registration.php");
     }
+    
+
+    //выполняет регистрацию нового акк
+    function register(){
+
+        require("app/models/account_model.php");
+        $this->model = new Account_model();
+
+        $this->model->registration();
+        $this->model->login();
+
+        header("Location: /main/");
+        exit();
+
+    }
+
+
+    //выполняет вход в существующий акк
+    //выводит сообщение об удачном / неудачном входе
+    function login(){
+        
+        require("app/models/account_model.php");
+        $this->model = new Account_model();
+
+        $this->model->login();
+        
+        header("Location: /main/");
+        exit();
+
+    }
+
 }
