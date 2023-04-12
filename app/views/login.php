0f13e92833dfe90dcee073ab26eb58f4d14d94a7
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-04-12 20:51:12 +0300

    log

--- app/views/login.php
+++ app/views/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Авторизация</title>
+    <title>Вход</title>
 
     <link rel="stylesheet" href="/css/popup.css">
 
@@ -58,77 +58,5 @@
         </div>
     </div>
 
-    <!-- <div id="popup_registration" class="popup">
-        <div class="popup__body">
-            <div class="popup__contetnt">
-                
-                <div class="popup__title">
-                    <a href="#popup" class="entrance">Вход</a>
-                    <a href="#" class="registration">Регистрация</a>
-                </div>
-
-                <div class="popup__middle__2">
-                    <form method="post" action="/main/register">
-                        <div class="input__row">
-                            <label for="UserName"></label>
-                            <input id="UserName" type="text" name="name" placeholder="Введите Ваше имя/ник">
-                        </div>
-
-                        <div class="input__row">
-                            <label for="UserAge"></label>
-                            <input id="UserAge" type="text" name ="birth" placeholder="Дата Вашего рождения">
-                        </div>
-                        
-                        <div class="input__row">
-                            <label for="UserSex"></label>
-                                <input id="UserSex" type="text" name="sex" placeholder="Укажите Ваш пол">
-                        </div>
-
-                        <div class="input__row">
-                            <label for="UserTarget"></label>
-                                <input id="UserTarget" type="text" name="goal" placeholder="Опишите Вашу цель">
-                        </div>
-                      
-
-                        <div class="input__row">
-                            <label for="UserProblem"></label>
-                            <input id="UserProblem" type="text" name="problem" placeholder="Опишите Вашу проблему">
-                        </div>
-
-                        <div class="input__row">
-                            <label for="UserEmail"><img src="/images/headForPopup.svg" alt=""></label>
-                            <input id="UserEmail" type="email" name="email" placeholder="Введите Email">
-                        </div>
-
-                        <div class="input__row">
-                            <label for="UserPassword"><img src="/images/lockForPopup.svg" alt=""></label>
-                            <input id="UserPassword" type="password" name="password" placeholder="Введите пароль">
-                        </div>
-
-                        <div class="input__row">
-                            <label for="UserPassword"><img src="/images/lockForPopup.svg" alt=""></label>
-                            <input id="UserPassword" type="password" name="checkpassword" placeholder="Подтвердите пароль">
-                        </div>
- 
-                        <div class="text">
-                            * Любую информацию, которую вы посчитаете личной, Вы сможете скрыть в профиле.
-                        </div>
-
-                        <button type="submit">Войти</button>
-
-                    </form>
-
-
-
-                </div>
-
-                
-                <div class="popup__footer">
-                </div>
-
-            </div>
-            
-        </div>
-    </div> -->
 </body>
 </html>
--- app/views/registration.php
+++ app/views/registration.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Авторизация</title>
+    <title>Регистрация</title>
 
     <link rel="stylesheet" href="/css/popup.css">
 
@@ -13,50 +13,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;200;300;400&display=swap" rel="stylesheet">
 </head>
 <body>
-        <!-- попап регистрации и входа
-        <div id="popup" class="popup">
-        <div class="popup__body">
-            <div class="popup__contetnt">
-                
-                <div class="popup__title">
-                    <a href="#" class="entrance">Вход</a>
-                    <a href="#popup_registration" class="registration">Регистрация</a>
-                </div>
-
-                <div class="popup__middle">
-                    <form method="post" action="/main/login">
-                        <div class="input__row">
-                            <label for="UserEmail"><img src="/images/headForPopup.svg" alt=""></label>
-                            <input id="UserEmail" type="email" name="email" placeholder="Введите Email">
-                        </div>
-
-                        <div class="input__row">
-                            <label for="UserPassword"><img src="/images/lockForPopup.svg" alt=""></label>
-                            <input id="UserPassword" type="password" name="password" placeholder="Введите пароль">
-                        </div>
-
-                        <div class="checkbox__row">
-                            <label class="rememberMe">
-                                <input type="checkbox" id="save" name="saveMe"> 
-                                Запомнить меня
-                            </label>
-                            <a href="#" class="forgot">Забыли пароль?</a>
-                        </div>
-
-                        <button type="submit">Войти</button>
- 
-                    </form>
-                </div>
-
-                
-                <div class="popup__footer">
-
-                </div>
-
-            </div>
-            
-        </div>
-    </div> -->
 
     <div id="popup_registration" class="popup">
         <div class="popup__body">
