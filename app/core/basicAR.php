20758b533887ac7ccb4ffb45843d97a1da1ac553
andrew <andreyhorolsky@gmail.com>
2023-04-13 09:31:35 +0300

    basic ar

---
+++ app/core/basicAR.php
@@ -0,0 +1,19 @@
+<?php
+
+class basicAR{
+    
+    static private $table;
+    static private $pdo;
+
+    
+    //создание подключения к бд
+    static function createConnect(){
+        if (!static::$pdo)
+            try{
+                static::$pdo = new PDO("mysql:host=localhost;dbname=eatNormal", "root", "123");
+            } catch (PDOException $ex){
+                static::$pdo = new PDO("mysql:host=localhost;dbname=eatNormal", "root");
+            }
+    }
+
+}
