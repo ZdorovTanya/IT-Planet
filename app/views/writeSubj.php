3fc082130f5798dfc03fe855006e277b2fa5523b
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-04-20 17:56:33 +0300

    ~ add article btn

--- app/views/writeSubj.php
+++ app/views/writeSubj.php
@@ -13,44 +13,41 @@
 <body>
     <div class="wrapper">
 
-        <?
-            include("app/views/menu.php");
-        ?>
+        <? include("app/views/menu.php"); ?>
 
         <div class="subjform">
+            <div class="_container">
+                <div class="subjform__body">
+                    <form action="/forum/saveSubj" method="post">
 
-            <form action="/forum/saveSubj" method="post">
+                        <label for="subject1">Анорексия</label>
+                        <input type="checkbox" name="subjects[]" id="subject1" value="1">
 
-                <label for="subject1">Анорексия</label>
-                <input type="checkbox" name="subjects[]" id="subject1" value="1">
-
-                <label for="subject2">Булимия</label>
-                <input type="checkbox" name="subjects[]" id="subject2" value="2">
-                
-                <label for="subject3">Оторексия</label>
-                <input type="checkbox" name="subjects[]" id="subject3" value="3">
-                
-                <label for="subject4">Компульсивное переедание</label>
-                <input type="checkbox" name="subjects[]" id="subject4" value="4">
-                
-                <label for="subject5">Другое</label>
-                <input type="checkbox" name="subjects[]" id="subject5" value="5">
+                        <label for="subject2">Булимия</label>
+                        <input type="checkbox" name="subjects[]" id="subject2" value="2">
+                        
+                        <label for="subject3">Оторексия</label>
+                        <input type="checkbox" name="subjects[]" id="subject3" value="3">
+                        
+                        <label for="subject4">Компульсивное переедание</label>
+                        <input type="checkbox" name="subjects[]" id="subject4" value="4">
+                        
+                        <label for="subject5">Другое</label>
+                        <input type="checkbox" name="subjects[]" id="subject5" value="5">
 
 
-                <label for="title">Название</label>
-                <input type="text" name="title" id="title">
-                
-                <label for="maintext">Текст</label>
-                <input type="text" name="maintext" id="maintext">
+                        <label for="title">Название</label>
+                        <input type="text" name="title" id="title">
+                        
+                        <label for="maintext">Текст</label>
+                        <input type="text" name="maintext" id="maintext">
 
-                <button type="submit">Готово!</button>
+                        <button type="submit">Готово!</button>
 
-            </form>
-
+                    </form>
+                </div>
+            </div>
         </div>
-
-
-
     </div>
 </body>
 </html>
