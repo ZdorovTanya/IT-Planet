ce9a79b297655558be9af20b734c364644434f86
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-05-29 22:45:47 +0300

    clinics

--- app/views/consultation.php
+++ app/views/consultation.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Консультации</title>
 
-    <link rel="stylesheet" href="/css/self-help.css">
+    <link rel="stylesheet" href="/css/consultation.css">
     <!-- <link rel="stylesheet" href="/css/popup.css"> -->
+
 
     <link rel="icon" href="/images/favicon.svg" type="image/svg+xml">
 
@@ -19,12 +20,53 @@
             include("app/views/menu.php");
         ?>
 
-        <div class="about-page _container _box">
-            <div class="about-page__title _title">
-                Страница находится в разработке
-            </div>
-        </div>
+        <main>
+            <div class="_container-consultation">
+                <div class="about-page _box">
+                    
+                    <div class="_title">
+                        Консультация
+                    </div>
+                    <div class="about-page__text _text">
+                        В случае если у Вас возникнет потребность в квалифицированной помощи и 
+                        Вы решите обратиться к специалисту, раздел консультаций окажет содействие в подборе психотерапевта или 
+                        группы поддержки.
+                        Также Вы сможете оставить заявку на первую бесплатную консультацию и 
+                        получить рекомедации по выбору эксперта.
+                        
+                    </div>
+                    <a href="#" class="consultation_btn"><img src="/images/consultation_btn.svg" alt=""></a>
+                </div>
 
+                <div class="life_help _box">
+                    <div class="_title">
+                        Очная помощь
+                    </div>
+                    <div class="about-life-help _text">
+                        Если Вам кажется, что Вы не в силах справиться 
+                        самостоятельно и Вам или Вашему близкому необходимо оказние постоянной помощи, 
+                        то в данном блоке Вы сможете обратиться к частным российским клиникам, которые направленны на лечение РПП.
+                    </div>
+                    <div class="clinics">
+                        <div class="clinics__header">
+                            <div class="name">Клиника расстройств пищевого поведения</div>
+                            <div class="line"></div>
+                            <div class="place"> 
+                                <img src="/images/whereClinics.svg" alt=""> 
+                                <div class="city">Москва</div>
+                            </div>
+                        </div>
+                        <div class="clinics__body">
+                            <div class="information _text">Основная деятельность клиники — лечение РПП — нервной анорексии и нервной булимии</div>
+                            <div class="line2"></div>
+                            <div class="picture"><img src="/images/clinics1.svg" alt=""></div>
+                        </div>
+                        <div class="btn__more">Узнать подробнее</div>
+                    </div>
+
+
+                </div>
+        </main>
     </div>
 
     <script src="/js/taskSpoiler.js"></script>
